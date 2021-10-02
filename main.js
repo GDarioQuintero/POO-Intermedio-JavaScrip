490aba9fcac07400cf94fd1429d823b14bbd5813
GermanQ <ing.germanquintero@gmail.com>
2021-10-01 21:45:12 -0500

    Agregando archivos del curso

---
+++ main.js
@@ -0,0 +1,12 @@
+const juan = {
+    name: "Juanito",
+    age: "18",
+    approvedCourses: ["Curso 1"],
+    addCourses(newCourse) {
+        this.approvedCourses.push(newCourse);
+    }
+};
+
+console.log(Object.keys(juan));
+console.log(Object.getOwnPropertyNames(juan));
+console.log(Object.entries(juan));
